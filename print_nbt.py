1842b633c76019a01b00b67c1176f1cfd16b9aae
Ole Martin Bjørndalen <ombdalen@gmail.com>
2014-02-16 22:21:54 +0100

    Added examples/print_dat.py.

--- print_nbt.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import sys
-from mcdata import _nbt_dict as nbt
-
-if __name__ == '__main__':
-    for filename in sys.argv[1:]:
-        data = nbt.load(filename)
-        print(nbt.encode_json(data))
