4123d6ff4771da2e6128865f0857cb059a045d8f
Ole Martin Bjørndalen <ombdalen@gmail.com>
2014-02-16 23:11:23 +0100

    Changed output format of print_dat.py.

--- examples/print_dat.py
+++ examples/print_dat.py
@@ -9,4 +9,7 @@ from mcdata import nbt
 if __name__ == '__main__':
     for filename in sys.argv[1:]:
         for tag, path in nbt.load(filename).walk():
-            print(path, tag)
+            if tag.type in ['compound', 'list']:
+                print(path, tag.type)
+            else:
+                print(path, tag.type, tag.value)
