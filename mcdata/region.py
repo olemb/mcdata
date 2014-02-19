f331f3b5f34af89af326b031de816b3f3eb61e24
Ole Martin Bjørndalen <ombdalen@gmail.com>
2014-02-19 07:37:28 +0100

    Cleanup in RegionFile.

--- mcdata/region.py
+++ mcdata/region.py
@@ -12,6 +12,7 @@ Todo:
 * "pos" is a bad name.
 """
 from __future__ import division
+import os as _os
 import math as _math
 import zlib as _zlib
 from . import nbt as _nbt
@@ -53,41 +54,57 @@ def read_int(infile, size):
 
 class RegionFile(object):
     # Todo: file object instead of filename and mode?
-    def __init__(self, filename, mode='rb'):
-        self.file = open(filename, mode)
+    def __init__(self, filename, mode='r'):
+        # Todo: accept file like object.
+        self.mode = mode
+        self.closed = False
 
         # The first two sectors are chunk headers, so they
         # are marked as used.
         self._sector_usage = SectorUsage([1, 1])
-        self._headers = []
+        self._chunks = []
 
-        self._load_headers()
+        file_existed = _os.path.exists(filename)
+        self.file = open(filename, {'r': 'rb', 'w': 'wb'}[mode])
+
+        if file_existed:
+            self._load_headers()
+        else:
+            self._init_headers()
+
+    def _init_headers(self):
+        self._chunks.extend({'offset': 0,
+                             'sector_count': 0,
+                             'timestamp': 0} for _ in range(MAX_LENGTH))
 
     def _load_headers(self):
         # Rewind in case we're called more than once.
         self.file.seek(0)
 
         for i in range(NUM_CHUNKS):
-            header = {'offset': read_int(self.file, 3),
+            chunk = {'offset': read_int(self.file, 3),
                       'sector_count': read_int(self.file, 1)}
-            self._headers.append(header)
+            self._chunks.append(chunk)
 
-            if header['offset']:
-                self._sector_usage.mark(header['offset'], header['sector_count'])
+            if chunk['offset']:
+                self._sector_usage.mark(chunk['offset'], chunk['sector_count'])
 
         # Load timestamps.
         for i in range(NUM_CHUNKS):
-            self._headers[i]['timestamp'] = read_int(self.file, 4)
+            self._chunks[i]['timestamp'] = read_int(self.file, 4)
+
+    def _save_headers(self):
+        raise NotImplemented
 
     def __getitem__(self, index):
         # Todo: this test is already done in __iter__().
         # Also, what should happen if the chunk doesn't exist?
         # (Exception probably.)
-        header = self._headers[index]
-        if header['offset'] == 0:
+        chunk = self._chunks[index]
+        if chunk['offset'] == 0:
             return None
 
-        self.file.seek(header['offset'] * SECTOR_SIZE)
+        self.file.seek(chunk['offset'] * SECTOR_SIZE)
 
         length = read_int(self.file, 4)
         # Todo: what if compression is not zlib?
@@ -96,28 +113,36 @@ class RegionFile(object):
 
         return _nbt.decode(data)
 
-    # def save_chunk(self, chunk):
-    #     pass
+    def __setitem__(self, chunk):
+        # Todo: which exception?
+        raise Exception('not implemented')
 
-    def delete_chunk(self, index):
+    def __delitem__(self, index):
         # Todo:
-        header = self._headers[index]
-        if header['offset']:
-            self._sector_usage.free(header['offset'], header['sector_count'])
-            header['offset'] = header['sector_count'] = 0
+        chunk = self._chunks[index]
+        if chunk['offset']:
+            self._sector_usage.free(chunk['offset'], chunk['sector_count'])
+            chunk['offset'] = chunk['sector_count'] = 0
             # Todo: clear data?
 
     def __iter__(self):
-        for index, header in enumerate(self._headers):
-            if header['offset']:
+        for index, chunk in enumerate(self._chunks):
+            if chunk['offset']:
                 yield self[index]
 
     def __len__(self):
         return MAX_CHUNKS
 
+    def close(self):
+        # self._save_headers()
+        if not self.closed:
+            if 'w' in self.mode: 
+                self._save_headers()
+            self.file.close()
+
     def __enter__(self):
         return self
 
     def __exit__(self, type, value, traceback):
-        self.file.close()
+        self.close()
         return False
